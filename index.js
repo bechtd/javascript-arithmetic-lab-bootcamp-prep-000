7af3c5f9e7ebce2c2e7701fe8c6b4e21a50ab399
David Becht <bechtd@gmail.com>
2018-04-17 16:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,25 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return a + b
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n)
 }
